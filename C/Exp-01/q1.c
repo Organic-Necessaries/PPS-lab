8f7289b01c47ebd17be7ade6aab1e436d76c5cd9
Sharaan <jalajasipuden@gmail.com>
2025-09-08 14:37:12 +0530

    Update q1.c

--- C/Exp-01/q1.c
+++ C/Exp-01/q1.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+  printf("###########################################################");
+  printf("\n                    Welcome!                               ");
+  printf("\nPlease choose a number from the following options:");
+  printf("\n1. Play the game!");
+  printf("\n2. Demo the game!");
+  printf("\n3. Exit");
+  printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+  return 0;
+}
